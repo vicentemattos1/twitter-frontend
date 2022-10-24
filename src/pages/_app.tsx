f1757fe96eab85e8bd785c0906b89a604029feb4
Vicente Mattos <vicentemattosf@gmail.com>
2022-10-24 09:54:04 -0300

    Setup initial React Modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from "next/app";
 import { UserProvider } from "../contexts/UserContext";
 import { PostsProvider } from "../contexts/PostsContext";
+import Modal from "react-modal";
 import { Sidebar } from "../components/Sidebar";
 
 import styles from "../styles/pages/Layout.module.scss";
 import "../styles/globals.scss";
+
+Modal.setAppElement("#__next");
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
