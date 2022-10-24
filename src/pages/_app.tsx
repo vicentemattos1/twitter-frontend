7461a8a7ece7d3431cf22938c19c5a18e926616b
Vicente Mattos <vicentemattosf@gmail.com>
2022-10-24 09:16:55 -0300

    Calling UserContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
-import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { UserProvider } from "../contexts/UserContext";
+import { Sidebar } from "../components/Sidebar";
 
 import styles from "../styles/pages/Layout.module.scss";
-import { Sidebar } from "../components/Sidebar";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={styles["container"]}>
-      <Sidebar />
-      <Component {...pageProps} />
-    </div>
+    <UserProvider>
+      <div className={styles["container"]}>
+        <Sidebar />
+        <Component {...pageProps} />
+      </div>
+    </UserProvider>
   );
 }
 
